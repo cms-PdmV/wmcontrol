ba4f912c9cf5eca4387632a6426f673843dcf793
franzoni <franzoni@4525493e-7705-40b1-a816-d608a930855b>
2012-12-13 20:13:53 +0000

    rereco muegamma 2012AB

---
+++ configs/FullReRecos2012/rereco_2011AB_mueg.py
@@ -0,0 +1,68 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: reco -s RAW2DIGI,L1Reco,RECO --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent AOD --no_exec --python_filename=rereco_2011AB_mueg.py --conditions FT_R_42_V24::All --filein /store/data/Run2011A/SingleMu/RAW/v1/000/175/770/DED83E9A-18D9-E011-AC08-BCAEC5329732.root -n 5
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/data/Run2011A/SingleMu/RAW/v1/000/175/770/DED83E9A-18D9-E011-AC08-BCAEC5329732.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.303.2.7 $'),
+    annotation = cms.untracked.string('reco nevts:5'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.AODoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODEventContent.outputCommands,
+    fileName = cms.untracked.string('reco_RAW2DIGI_L1Reco_RECO.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('RECO')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'FT_R_42_V24::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.AODoutput_step = cms.EndPath(process.AODoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.AODoutput_step)
