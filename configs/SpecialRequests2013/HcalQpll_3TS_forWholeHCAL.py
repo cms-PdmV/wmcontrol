b5ff725aef227c1c9fc30e6232f5e476de968518
franzoni <franzoni@4525493e-7705-40b1-a816-d608a930855b>
2013-01-10 19:43:01 +0000

    hopefully the last episode of this saga; https://hypernews.cern.ch/HyperNews/CMS/get/dataopsrequests/2074/1/1/1/1/1/2.html

---
+++ configs/SpecialRequests2013/HcalQpll_3TS_forWholeHCAL.py
@@ -0,0 +1,86 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.6 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: HcalQpll_3TS_wholeHCAL_13Jan10 -s RAW2DIGI,L1Reco,RECO,DQM --processName RECO --data --scenario pp --datatier RECO,DQM --eventcontent RECO,DQM --conditions GR_P_V41::All --python_filename HcalQpll_3TS_forWholeHCAL.py --no_exec --custom_conditions=HcalRecoParams_special_3TS,HcalRecoParamsRcd,frontier://FrontierProd/CMS_COND_44X_HCAL --filein /store/data/Run2012C/JetHT/RAW/v1/000/199/812/0C450FA0-6BD8-E111-BB28-BCAEC5329727.root -n 10
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('DQMOffline.Configuration.DQMOffline_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('/store/data/Run2012C/JetHT/RAW/v1/000/199/812/0C450FA0-6BD8-E111-BB28-BCAEC5329727.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.6 $'),
+    annotation = cms.untracked.string('HcalQpll_3TS_wholeHCAL_13Jan10 nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOEventContent.outputCommands,
+    fileName = cms.untracked.string('HcalQpll_3TS_wholeHCAL_13Jan10_RAW2DIGI_L1Reco_RECO_DQM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('RECO')
+    )
+)
+
+process.DQMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.DQMEventContent.outputCommands,
+    fileName = cms.untracked.string('HcalQpll_3TS_wholeHCAL_13Jan10_RAW2DIGI_L1Reco_RECO_DQM_inDQM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('DQM')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'GR_P_V41::All'
+process.GlobalTag.toGet = cms.VPSet()
+process.GlobalTag.toGet.append(cms.PSet(tag=cms.string("HcalRecoParams_special_3TS"),record=cms.string("HcalRecoParamsRcd"),connect=cms.untracked.string("frontier://FrontierProd/CMS_COND_44X_HCAL"),))
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.dqmoffline_step = cms.Path(process.DQMOffline)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.dqmoffline_step,process.endjob_step,process.RECOoutput_step,process.DQMoutput_step)
+
