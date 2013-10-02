e221368ad5330691d570937e5279abf7746c20de
PdmV Service <pdmv.service@cern.ch>
2013-10-02 18:32:32 +0200

    wmtest option for upload

--- wmupload.py
+++ wmupload.py
@@ -11,11 +11,18 @@ def prepare_parser():
                       default=os.environ['WMCONTROL_GROUP'] if os.environ.has_key('WMCONTROL_GROUP') else '')
     parser.add_option('-l', '--label', help='Label and description of config', dest='label',
                       default='Uploaded through wmupload')
+    parser.add_option('--wmtest', help='To inject requests to the cmsweb test bed', action='store_true' ,
+                      dest='wmtest', default=False)
+    parser.add_option('--wmtesturl', help='To inject to a specific testbed', dest='wmtesturl',
+                      default='cmsweb-testbed.cern.ch')
     return parser
 
 def main():
     parser = prepare_parser()
     options, args = parser.parse_args()
+    if options.wmtest:
+        print "Setting to injection in cmswebtest : ", options.wmtesturl
+        wma.testbed(options.wmtesturl)
     for cfg_name in args:
         wma.upload_to_couch(cfg_name, options.label, options.user, options.group)
 
