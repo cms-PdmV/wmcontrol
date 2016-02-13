85a4e4e0fc0e6e0eec8db9be10ba44c41b94f64a
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2016-02-13 11:05:02 +0100

    checks can be removed given the order of AlCa valid instructions

--- relval_submit.py
+++ relval_submit.py
@@ -83,19 +83,19 @@ def main():
         logging.error('Need to suply a metadata text file name')
         return -3
         
-    # check that user has properly set up the WMA libraries in PYTHONPATH
-    try:
-        pypath = os.environ['PYTHONPATH']
-    except:
-        print "no PYTHONPATH defined in your environment; exiting"
-        sys.exit()
-    else:
-        if len ( filter( lambda r : "WMCore" in r, pypath.split(":") ) ) > 0 :
-            print "hapy"
-            pass
-        else :
-            print "no WMCore present in your PYTHONPATH; exiting"
-            sys.exit()
+    ## check that user has properly set up the WMA libraries in PYTHONPATH
+    #try:
+    #    pypath = os.environ['PYTHONPATH']
+    #except:
+    #    print "no PYTHONPATH defined in your environment; exiting"
+    #    sys.exit()
+    #else:
+    #    if len ( filter( lambda r : "WMCore" in r, pypath.split(":") ) ) > 0 :
+    #        print "hapy"
+    #        pass
+    #    else :
+    #        print "no WMCore present in your PYTHONPATH; exiting"
+    #        sys.exit()
 
 
     if True:
