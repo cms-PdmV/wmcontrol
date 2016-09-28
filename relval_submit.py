ec0d00f110d4e1427235830393866a211ba8d8f8
Antanas Norkus <antanas.norkus@cern.ch>
2016-09-28 15:29:06 +0200

    Code pretify

--- relval_submit.py
+++ relval_submit.py
@@ -19,18 +19,17 @@ import json
 import errno
 import ast
 
-def execme(command,dryrun=False):    
+def execme(command, dryrun=False):
     '''Wrapper for executing commands.
     '''
     if dryrun:
         print command
     else:
-        print " * Executing: %s..."%command
+        print " * Executing: %s..." % (command)
         os.system(command)
         print " * Executed!"
 
-    
-def getInput(default, prompt = ''):
+def getInput(default, prompt=''):
     '''Like raw_input() but with a default and automatic strip().
     '''
 
@@ -40,8 +39,7 @@ def getInput(default, prompt = ''):
 
     return default.strip()
 
-
-def getInputChoose(optionsList, default, prompt = ''):
+def getInputChoose(optionsList, default, prompt=''):
     '''Makes the user choose from a list of options.
     '''
 
@@ -55,8 +53,7 @@ def getInputChoose(optionsList, default, prompt = ''):
         except IndexError:
             logging.error('The index you provided is not in the given list.')
 
-
-def getInputRepeat(prompt = ''):
+def getInputRepeat(prompt=''):
     '''Like raw_input() but repeats if nothing is provided and automatic strip().
     '''
 
@@ -67,23 +64,19 @@ def getInputRepeat(prompt = ''):
 
         logging.error('You need to provide a value.')
 
-
 def main():
     '''Entry point.
     '''
 
-    parser = optparse.OptionParser(usage =
-        'Usage: %prog <file> [<file> ...]\n'
-    )
-    
+    parser = optparse.OptionParser(usage='Usage: %prog <file> [<file> ...]\n')
     (options, arguments) = parser.parse_args()
 
-    try: 
+    try:
         metadataFilename = arguments[0]
     except IndexError:
         logging.error('Need to suply a metadata text file name')
         return -3
-        
+
     ## check that user has properly set up the WMA libraries in PYTHONPATH
     #try:
     #    pypath = os.environ['PYTHONPATH']
@@ -114,28 +107,28 @@ def main():
             # Wizard
             while True:
                 print '''\nWizard for metadata
-I will ask you some questions to fill the metadata file. For some of the questions there are defaults between square brackets (i.e. []), leave empty (i.e. hit Enter) to use them.''' 
+I will ask you some questions to fill the metadata file. For some of the questions there are defaults between square brackets (i.e. []), leave empty (i.e. hit Enter) to use them.'''
 
-                typeList = ['HLT+RECO','PR','HLT+RECO+ALCA','PR+ALCA']
-                
+                typeList = ['HLT+RECO', 'PR', 'HLT+RECO+ALCA', 'PR+ALCA']
+
                 print '\nTypes of workflow submissions'
                 for (index, type) in enumerate(typeList):
                         print '   %s) %s' % (index, type)
-                        
+
                 type = getInputChoose(typeList, '0', '\nWhich type of workflow submission\ntype [0]: ')
-                                    
+
                 if 'HLT+RECO' in type:
                     hlt_release = getInput('CMSSW_7_4_9_patch1', '\nWhich CMSSW release for HLT?\ne.g. CMSSW_7_4_9_patch1\nhlt_release [CMSSW_7_4_9_patch1]: ')
-                    
+
                 pr_release = getInput('CMSSW_7_4_11_patch1', '\nWhich CMSSW release for Prompt Reco?\ne.g. CMSSW_7_4_11_patch1\npr_release [CMSSW_7_4_11_patch1]: ')
 
                 if 'HLT+RECO' in type:
-                    hlt_menu  = getInput('orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7', '\nWhich HLT menu?\ne.g. orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7\nhlt_menu [orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7]: ')
-                
+                    hlt_menu = getInput('orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7', '\nWhich HLT menu?\ne.g. orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7\nhlt_menu [orcoff:/cdaq/physics/Run2015/25ns14e33/v3.5/HLT/V7]: ')
+
                 newgt = getInput('74X_dataRun2_HLTValidation_2015-09-07-08-26-15', '\nWhat is the new GT to be validated?\ne.g. 74X_dataRun2_HLTValidation_2015-09-07-08-26-15\nnewgt [74X_dataRun2_HLTValidation_2015-09-07-08-26-15]: ')
-                
+
                 gt = getInput('74X_dataRun2_HLT_v1', '\nWhat is the reference GT?\ne.g. 74X_dataRun2_HLT_v1\ngt [74X_dataRun2_HLT_v1]: ')
-                
+
                 if 'HLT+RECO' in type:
                     basegt = getInput('74X_dataRun2_Prompt_v1', '\nWhat is the common GT for Reco?\ne.g. 74X_dataRun2_Prompt_v1\nbasegt [74X_dataRun2_Prompt_v1]: ')
 
@@ -143,15 +136,15 @@ I will ask you some questions to fill the metadata file. For some of the questio
 
                 run_err_mess = 'The run value has to be an integer, a dictionary or empty (null).'
                 runORrunLs  = getInput('254906', '\nWhich run number or run number+luminosity sections?\ne.g. 254906 or\n     [254906,254905] or\n     {\'256677\': [[1, 291], [293, 390]]}\nrunORrunLs [254906]: ')
-                run   = ''
+                run = ''
                 runLs = ''
                 # do some type recognition and set run or runLs accordingly
                 try:
-                    if   isinstance(runORrunLs, str) and "{"  in runORrunLs :
+                    if isinstance(runORrunLs, str) and "{" in runORrunLs :
                         runLs = ast.literal_eval(runORrunLs)                              # turn a string into a dict and check it's valid
                     elif isinstance(runORrunLs, dict):
                         runLs = runORrunLs                                    # keep dict
-                    elif isinstance(runORrunLs, str) and "["  in runORrunLs :
+                    elif isinstance(runORrunLs, str) and "[" in runORrunLs :
                         run   = ast.literal_eval(runORrunLs)                              # turn a string into a list and check it's valid
                     elif isinstance(runORrunLs, str) :
                         run = int(runORrunLs)                                 # turn string into int
@@ -163,9 +156,8 @@ I will ask you some questions to fill the metadata file. For some of the questio
                     logging.error(run_err_mess)
 
 
-                b0T  = getInput('n', '\nIs this for B=0T?\nAnswer [n]: ')
-                
-                hion  = getInput('n', '\nIs this for Heavy Ions? Note B=0T is not compatible with Heavy Ions at the moment\nAnswer [n]: ')                
+                b0T = getInput('n', '\nIs this for B=0T?\nAnswer [n]: ')
+                hion = getInput('n', '\nIs this for Heavy Ions? Note B=0T is not compatible with Heavy Ions at the moment\nAnswer [n]: ')
 
                 metadata = {
                     'PR_release': pr_release,
@@ -174,9 +166,8 @@ I will ask you some questions to fill the metadata file. For some of the questio
                         'newgt': newgt,
                         'gt': gt,
                         'ds': ds,
-                        'run': run
-                    }
-                }
+                        'run': run}}
+
                 if runLs:
                     metadata['options']['runLs'] = runLs
                     metadata['options'].pop('run')
@@ -190,34 +181,32 @@ I will ask you some questions to fill the metadata file. For some of the questio
                     metadata['HLT_release'] = hlt_release
                     metadata['options'].update({
                         'HLT': 'Custom',
-                        'HLTCustomMenu': hlt_menu,                        
+                        'HLTCustomMenu': hlt_menu,
                         'basegt': basegt})
                     if metadata['PR_release'] != metadata['HLT_release']:
                         pr_release = metadata['PR_release']
-                        metadata['options']['recoCmsswDir'] = '../%s/' % pr_release
-                        
+                        metadata['options']['recoCmsswDir'] = '../%s/' % (pr_release)
 
-                metadata = json.dumps(metadata, sort_keys = True, indent = 4)
-                print '\nThis is the generated metadata:\n%s' % metadata
+                metadata = json.dumps(metadata, sort_keys=True, indent=4)
+                print '\nThis is the generated metadata:\n%s' % (metadata)
 
-                if getInput('n', '\nIs it fine (i.e. save in %s)?\nAnswer [n]: ' % metadataFilename).lower() == 'y':
+                if getInput('n', '\nIs it fine (i.e. save in %s)?\nAnswer [n]: ' % (metadataFilename)).lower() == 'y':
                     break
-                
+
             logging.info('Saving generated metadata in %s...', metadataFilename)
             with open(metadataFilename, 'wb') as metadataFile:
                 metadataFile.write(metadata)
                 logging.info('...%s file created.', metadataFilename)
-            
 
     with open(metadataFilename, 'rb') as metadataFile:
         metadata = json.loads(metadataFile.read())
         print '\nexecute the following commands:\n'
         commands = []
         try:
-            if metadata['HLT_release']:            
+            if metadata['HLT_release']:
                 #commands.append('eval \'scramv1 project %s\'' % metadata['HLT_release'] )
-                commands.append('scramv1 project %s' %metadata['HLT_release'])
-                commands.append('cd %s/src' % metadata['HLT_release'] )
+                commands.append('scramv1 project %s' % (metadata['HLT_release']))
+                commands.append('cd %s/src' % (metadata['HLT_release']))
                 #commands.append('eval \'scramv1 runtime -sh\'')
                 commands.append('cmsenv')
                 commands.append('git cms-addpkg HLTrigger/Configuration')
@@ -225,14 +214,14 @@ I will ask you some questions to fill the metadata file. For some of the questio
                 commands.append('scramv1 b')
                 commands.append('voms-proxy-init --voms cms')
                 commands.append('source /afs/cern.ch/cms/PPD/PdmV/tools/subSetup_slc6.sh')
-                commands.append('cd -')                
+                commands.append('cd -')
                 if metadata['PR_release'] != metadata['HLT_release']:
                     #commands.append('eval \'scramv1 project %s\'' % metadata['PR_release'])
-                    commands.append('scramv1 project %s' % metadata['PR_release'])
-        except KeyError:            
+                    commands.append('scramv1 project %s' % (metadata['PR_release']))
+        except KeyError:
             #commands.append('eval \'scramv1 project %s\'' % metadata['PR_release'] )
-            commands.append('scramv1 project %s' % metadata['PR_release'])
-            commands.append('cd %s/src' % metadata['PR_release'] )
+            commands.append('scramv1 project %s' % (metadata['PR_release']))
+            commands.append('cd %s/src' % (metadata['PR_release']))
             #commands.append('eval \'scramv1 runtime -sh\'')
             commands.append('cmsenv')
             commands.append('voms-proxy-init --voms cms')
@@ -243,15 +232,15 @@ I will ask you some questions to fill the metadata file. For some of the questio
         for key, val in metadata['options'].iteritems():
             # cond_submit_command += '--%s %s ' % ( key, val )
             if isinstance(val, list):
-                cond_submit_command += '--%s "' % ( key)
+                cond_submit_command += '--%s "' % (key)
                 for u in val:
-                    cond_submit_command += '%s,'%u
+                    cond_submit_command += '%s,' % (u)
                 cond_submit_command = cond_submit_command[:-1]
                 cond_submit_command += '" '
             elif isinstance(val, dict):
-                cond_submit_command += '--%s "%s" ' % ( key, val)
+                cond_submit_command += '--%s "%s" ' % (key, val)
             else:
-                cond_submit_command += '--%s %s ' % ( key, val )
+                cond_submit_command += '--%s %s ' % (key, val)
 
         commands.append(' git clone -b master git@github.com:cms-PdmV/wmcontrol.git  master-my-local-name    (**make sure that that PdmV master is the one you want to use**)')
         commands.append('cd wmcontrol')
@@ -276,20 +265,21 @@ I will ask you some questions to fill the metadata file. For some of the questio
 
         try:
             if metadata['HLT_release']:
-                commands.append('./wmcontrol.py --req_file HLTConditionValidation_%s_%s_%s.conf |& tee wmcontrol.HLT.1.log' % (metadata['HLT_release'], metadata['options']['basegt'], run_label_for_fn ) )
+                commands.append('./wmcontrol.py --req_file HLTConditionValidation_%s_%s_%s.conf |& tee wmcontrol.HLT.1.log' % (
+                        metadata['HLT_release'], metadata['options']['basegt'], run_label_for_fn))
         except KeyError:
-            commands.append('./wmcontrol.py --req_file PRConditionValidation_%s_%s_%s.conf |& tee wmcontrol.PR.1.log' % (metadata['PR_release'], metadata['options']['newgt'], run_label_for_fn ) )
+            commands.append('./wmcontrol.py --req_file PRConditionValidation_%s_%s_%s.conf |& tee wmcontrol.PR.1.log' % (
+                        metadata['PR_release'], metadata['options']['newgt'], run_label_for_fn))
+
         commands.append('rm *.couchID')
 
         dryrun = True
         # now execute commands
         for command in commands:
-            execme(command,dryrun)
+            execme(command, dryrun)
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        format = '[%(asctime)s] %(levelname)s: %(message)s',
-        level = logging.INFO,
-    )
+    logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
+            level=logging.INFO)
 
     sys.exit(main())
