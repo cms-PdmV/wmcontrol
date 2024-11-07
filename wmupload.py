cdc6e6bb334d290b7a2cb44df67eb78fcc7c1638
Geovanny Gonzalez <geovanny.gonzalez@cern.ch>
2024-11-07 12:25:45 +0100

    Remove dependencies between cms-sw releases and ReqMgr2 config cache
    Allow to create a tweak for a cms-sw configuration fragment with one
    specific cms-sw release and submit the configuration to the ReqMgr2 cache
    using another.

--- wmupload.py
+++ wmupload.py
@@ -16,16 +16,34 @@ def prepare_parser():
                       dest='wmtest', default=False)
     parser.add_option('--wmtesturl', help='To inject to a specific testbed', dest='wmtesturl',
                       default='cmsweb-testbed.cern.ch')
+    parser.add_option('--create-tweak',
+                      help='Instead of performing the submission directly, tweak the parameters and just save them as JSON',
+                      dest='create_tweak',
+                      default=False,
+                      action='store_true')
+    parser.add_option('--from-tweak',
+                      help='Submit the configuration to the ReqMgr2 cache loading tweaked parameters from a JSON file, instead of loading the module',
+                      dest='from_tweak',
+                      default=False,
+                      action='store_true')
     return parser
 
 def main():
     parser = prepare_parser()
     options, args = parser.parse_args()
+
+    if options.create_tweak and options.from_tweak:
+        raise RuntimeError('--create-tweak and --from-tweak are mutually exclusive options. Select only one of them')
     if options.wmtest:
         print("Setting to injection in cmswebtest : ", options.wmtesturl)
         wma.testbed(options.wmtesturl)
+
     for cfg_name in args:
-        wma.upload_to_couch(cfg_name, options.label, options.user, options.group)
+        if options.create_tweak:
+            wma.tweaks_to_file(cfg_name)
+        else:
+            tweaks = wma.from_tweaks_file(cfg_name) if options.from_tweak else {}
+            wma.upload_to_couch(cfg_name, options.label, options.user, options.group, tweaks=tweaks)
 
 if __name__ == "__main__":
     main()
